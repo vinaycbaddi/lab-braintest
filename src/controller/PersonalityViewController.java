63a7d9441d447896cfb0cc6425e38015787b01bd
root <root@LAPTOP-ODE2T0U9.localdomain>
2020-07-13 23:15:05 +0530

    Vinay(S-55)

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
+
 
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
@@ -33,8 +35,9 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
+		PersonalityCalculator a=new PersonalityCalculator();
+		String message=a.findYourBrainType(options);
 				
-		/* 
 
 		request.setAttribute("message", message);
 		
@@ -53,7 +56,7 @@ public class PersonalityViewController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		
-		}*/
+		}
 		}
 	}
 
--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,31 @@
 package service;
 
+public class PersonalityCalculator{
+	public String findYourBrainType(String options) {
+		int[] array=findAnswers(options);
+		int a=0,b=0,x=0;
+		a=array[0]+array[1]+array[2]+array[4]+array[7]+array[9]+array[10]+array[11]+array[13]+array[17]+array[19];
+		b=array[3]+array[5]+array[6]+array[8]+array[12]+array[14]+array[15]+array[16]+array[18];
+		x=66-a+b;
+		if(x>=20&&x<=55) {
+			return "leftbrained";
+		}
+		else if(x>=56&&x<=64) {
+			return "noclearpreference";
+		}
+		else if(x>=65&&x<=100){
+			return "rightbrained";
+		}
+		else
+			return "false";
+		
+	}
+	public int[] findAnswers(String options){
+		String[] str=options.split(",");
+		int[] array=new int[str.length];
+		for(int i=0;i<array.length;i++) {
+			array[i]=Integer.parseInt(str[i]);
+		}
+		return array;
+	}
+}
